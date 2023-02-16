ddd12519240e796554ce2a4111d1a20b81a77ab1
William Gibson <william@darn.fish>
2023-02-16 02:51:16 -0500

    Don't commit lib

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import EventEmitter from 'eventemitter3'
-import { useThree as _useThree } from '@react-three/fiber'
-export interface SyncedCameraStore {
-    emitter: EventEmitter;
-}
-export declare const useSyncedCameraStore: import('zustand').UseBoundStore<import('zustand').StoreApi<SyncedCameraStore>>
-export default function useSyncedCamera(useThree: typeof _useThree): () => void
